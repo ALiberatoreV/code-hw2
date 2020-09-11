1e7293b6228cf70dd525a35fc325a7a3a0818700
ALiberatoreV <70169681+ALiberatoreV@users.noreply.github.com>
2020-09-10 23:58:54 -0400

    commit 2

---
+++ srcipt.js
@@ -0,0 +1,13 @@
+var firstName = "Anthony";
+var lastName = "Liberatore";
+var age = 21;
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old");
+console.log("doing makes learning fun best class yet");
+ if(age > 21 ){
+    alert("I am legally able to drink in the states.")
+  } else if(age == 21){
+      alert("I am just barely able to drink legally in the states." )
+  } else if(age <= 21){
+      alert("It is illegal for me to drink in the states.")
+  }
